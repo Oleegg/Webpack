fda1eb4964898af80a9b9bb145970ecd95285bab
Oleegg <91334112+Oleegg@users.noreply.github.com>
2022-03-13 22:35:01 +0300

    feat: add styles and bgc

--- src/index.js
+++ src/index.js
@@ -1,2 +1,18 @@
 import 'normalize.css';
-console.log(1);
+import './styles/style.scss';
+
+const navList = document.querySelector('.nav__list')
+const burger = document.querySelector('.burger')
+const navItems = document.querySelectorAll('.nav__item')
+
+burger.addEventListener('click', openMenu)
+navItems.forEach(el => el.addEventListener('click', closeMunu))
+
+function openMenu() {
+    burger.classList.toggle('open')
+    navList.classList.toggle('open')
+}
+function closeMunu() {
+    burger.classList.remove('open')
+    navList.classList.remove('open')
+}
